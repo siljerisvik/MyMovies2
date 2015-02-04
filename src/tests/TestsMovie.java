258ee13e32633099d9687774dcc3b058ce52898d
Jim Frode Nilsen <jimnil93@hotmail.com>
2015-02-04 22:32:09 +0100

    Added new changes.
    New changes, mostly to Movie.java, some new functionality to
    WebRepository.java and new tests for Tests.movie. Assignment 1, part 1 &
    2 should be mostly complete.

--- src/tests/TestsMovie.java
+++ src/tests/TestsMovie.java
@@ -1,6 +1,12 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import it.jtomato.gson.AbridgedCast;
 import it.jtomato.gson.Rating;
 
 import org.junit.Test;
@@ -8,6 +14,12 @@ import org.junit.Test;
 import business.Movie;
 
 public class TestsMovie {
+	
+	@Test
+	public void constructor_NoArgs_ObjectExists() {
+		Movie movie = new Movie(new it.jtomato.gson.Movie());
+		assertNotNull(movie);	
+	}
 
 	@Test
 	public void getTitle_GetsCorrectTitle() {
@@ -43,12 +55,12 @@ public class TestsMovie {
 	}
 	
 	@Test
-	public void getRating_getsCorrectRating() {
+	public void getCriticsScore_getsCorrectRating() {
 		it.jtomato.gson.Movie m = new it.jtomato.gson.Movie();
 		m.rating = new Rating();
 		m.rating.criticsScore = 100;
 		Movie movie = new Movie(m);
-		assertEquals(100, movie.getRating());
+		assertEquals(100, movie.getCriticsScore());
 	}
 	
 	@Test
@@ -58,5 +70,57 @@ public class TestsMovie {
 		Movie movie = new Movie(m);
 		assertEquals("119", movie.getRuntime());
 	}
-
+	
+	@Test
+	public void getAudienceRating_returnsCorrect() {
+		it.jtomato.gson.Movie m = new it.jtomato.gson.Movie();
+		m.rating = new Rating();
+		m.rating.audienceRating = "Awesome";
+		Movie movie = new Movie(m);
+		assertEquals("Awesome", movie.getAudienceRating());
+		
+	}
+	
+	@Test
+	public void getAudienceScore_returnsCorrect() {
+		it.jtomato.gson.Movie m = new it.jtomato.gson.Movie();
+		m.rating = new Rating();
+		m.rating.audienceScore = 99;
+		Movie movie = new Movie(m);
+		assertEquals(99, movie.getAudienceScore());
+	}
+	
+	@Test
+	public void getMpaaRating_returnsCorrect() {
+		it.jtomato.gson.Movie m =new it.jtomato.gson.Movie();
+		m.mpaaRating = "Adults only";
+		Movie movie = new Movie(m);
+		assertEquals("Adults only", movie.getMpaaRating());
+	}
+	
+	@Test
+	public void getGenre_returnsCorrect() {
+		it.jtomato.gson.Movie m = new it.jtomato.gson.Movie();
+		List<String> test = new ArrayList<String>();
+		test.add("Action");
+		test.add("Sports");
+		m.genres = test;
+		Movie movie = new Movie(m);
+		assertEquals(Arrays.asList("Action", "Sports"), movie.getGenre());
+	}
+	@Test
+	public void getAbridgedCast_returnsCorrect() {
+		it.jtomato.gson.Movie m = new it.jtomato.gson.Movie();
+		m.abridgedCast = new ArrayList<AbridgedCast>();
+		AbridgedCast cast = new AbridgedCast();
+		cast.name = "Per Robert";
+		AbridgedCast cast2 = new AbridgedCast();
+		cast2.name = "Svein Are";
+		m.abridgedCast.add(cast);
+		m.abridgedCast.add(cast2);
+		Movie movie = new Movie(m);
+		assertEquals(Arrays.asList("Per Robert","Svein Are"), movie.getAbridgedCast());
+		
+	}
+	
 }
